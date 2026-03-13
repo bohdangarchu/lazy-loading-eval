32c102c78773a769370524f96719a1ab8cf400a4
Bohdan Garchu <bohdan.garchu@gmail.com>
2026-03-13 14:05:49 +0100

    tmp dir

--- experiments/experiment1/builder/run-all.sh
+++ experiments/experiment1/builder/run-all.sh
@@ -5,10 +5,19 @@ sudo ./clear-builder-cache.sh
 echo "[$(date '+%Y-%m-%d %H:%M:%S')] Starting build-base"
 sudo ./build-base.sh
 sudo ./clear-builder-cache.sh
+sleep 60
+
 echo "[$(date '+%Y-%m-%d %H:%M:%S')] Starting build-2dfs"
 ./build-2dfs.sh
 sudo ./clear-builder-cache.sh
+sleep 60
+
 echo "[$(date '+%Y-%m-%d %H:%M:%S')] Starting build-stargz"
 sudo ./build-stargz.sh
+sudo ./clear-builder-cache.sh
+sleep 60
+
 echo "[$(date '+%Y-%m-%d %H:%M:%S')] Starting build-zstdchunked"
 sudo ./build-zstdchunked.sh
+sudo ./clear-builder-cache.sh
+sleep 60
