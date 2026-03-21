dd6e1bdb1bd33a38f9cec66d1c78c0ababcbb286
Bohdan Garchu <bohdan.garchu@gmail.com>
2026-03-21 14:13:54 +0100

    no push

--- experiments/experiment1/builder/build-2dfs-stargz.sh
+++ experiments/experiment1/builder/build-2dfs-stargz.sh
@@ -6,4 +6,4 @@ eval "$(python3 "${SCRIPT_DIR}/../../load-schema.py" "${SCHEMA}")"
 export IMAGE=${REGISTRY_NODE}:5000/${IMG_2DFS_STARGZ_NAME}:${IMG_2DFS_STARGZ_TAG}
 
 time sudo TMPDIR=/mydata/tmp TDFS_HOME=/mydata/.2dfs tdfs build --platforms linux/amd64 --enable-stargz --force-http ${BASE_IMAGE} ${IMAGE}
-time sudo TMPDIR=/mydata/tmp TDFS_HOME=/mydata/.2dfs tdfs image push --force-http ${IMAGE}
+# time sudo TMPDIR=/mydata/tmp TDFS_HOME=/mydata/.2dfs tdfs image push --force-http ${IMAGE}
