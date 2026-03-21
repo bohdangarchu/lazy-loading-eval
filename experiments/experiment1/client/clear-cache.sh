f870314e66eb638915dbf7e8efed23fe003fa996
Bohdan Garchu <bohdan.garchu@gmail.com>
2026-03-21 14:31:58 +0100

    clear stargz cache

--- experiments/experiment1/client/clear-cache.sh
+++ experiments/experiment1/client/clear-cache.sh
@@ -3,10 +3,10 @@ set -euox pipefail
 STARGZ_ROOT="/var/lib/containerd-stargz-grpc"
 
 sudo systemctl stop containerd stargz-snapshotter
-sudo rm -rf /var/lib/containerd/io.containerd.content.v1.content/blobs/*
-sudo rm -rf /var/lib/containerd/io.containerd.content.v1.content/ingest/*
-sudo rm -rf /var/lib/containerd/io.containerd.snapshotter.v1.overlayfs/*
+sudo bash -c 'rm -rf /var/lib/containerd/io.containerd.content.v1.content/blobs/*'
+sudo bash -c 'rm -rf /var/lib/containerd/io.containerd.content.v1.content/ingest/*'
+sudo bash -c 'rm -rf /var/lib/containerd/io.containerd.snapshotter.v1.overlayfs/*'
 sudo rm -rf /var/lib/containerd/io.containerd.metadata.v1.bolt/meta.db
-sudo rm -rf "${STARGZ_ROOT:?}"/*
+sudo bash -c "rm -rf ${STARGZ_ROOT:?}/*"
 sudo systemctl start containerd stargz-snapshotter
 sleep 10
